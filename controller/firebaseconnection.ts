71ab8ea7d220a74a549309643ab61bab4eb4707d
CrazyOldBuffalo <59769679+CrazyOldBuffalo@users.noreply.github.com>
2025-06-25 22:18:25 +0100

    Fixes an issue with the ModuleSKills by moving interface to types, adds new controllers for firebase for signing components

---
+++ controller/firebaseconnection.ts
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+
+const config = {
+    
+}
+
+const app = initializeApp(config)
---
+++ controller/signing-controller.ts
@@ -0,0 +1,12 @@
+import type { FirebaseApp } from "firebase/app";
+import type {CollectionReference, DocumentData, Firestore} from 'firebase/firestore'
+import {getFirestore, collection, getDocs, } from 'firebase/firestore';
+
+
+async function GetDB(firebaseapp: FirebaseApp): Promise<Firestore> {
+    return getFirestore(firebaseapp)
+}
+
+async function GetCollection(collectionid: string, firestore: Firestore): Promise<CollectionReference<DocumentData, DocumentData>> {
+    return collection(firestore, collectionid)
+}
